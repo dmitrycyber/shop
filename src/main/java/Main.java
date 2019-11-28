a9001f21b47dc2f990cb4765df307e905b9c62ad
Dmitry <dmitry.ivoninsky@gmail.com>
2019-11-29 02:29:10 +0300

    Replacing project to new directory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("-----------TASK1------------");
+
         Shop shop1 = new Sosedi();
         Item mouse1 = new Mouse(1, "A4Tech", 10);
         Item mouse2 = new Mouse(2, "Logitech", 20);
